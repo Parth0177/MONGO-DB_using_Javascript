e96e93bfe78eaaa701dbeea083e31e1e6e6828f0
Parth Tiwari <parthtiwari670@gmail.com>
2025-06-22 23:52:45 +0530

    Add user schema and initial data insertion with Mongoose connection

---
+++ insert.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+main().then(()=>{
+  console.log('Sucessfully connected!');
+})
+.catch(err=> console.log(err));
+
+
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/Test')
+}
+
+const userSchema = new mongoose.Schema({
+  name:String,
+  email : String,
+  age : Number,
+});
+
+const User = mongoose.model('User' , userSchema);
+
+const user1 = new User({
+  name:"Parth",
+  email:"parth670@gmail.com",
+  age:22
+})
+
+user1.save();
