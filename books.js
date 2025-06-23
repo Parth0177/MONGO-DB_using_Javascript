8628e133c1f367c4dcaba32466d760d9e3b24a35
Parth Tiwari <parthtiwari670@gmail.com>
2025-06-24 00:48:58 +0530

    Add initial MongoDB connection and book schema definition

---
+++ books.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+main().then(()=>{
+  console.log('Sucessfully connected!');
+})
+.catch(err=> console.log(err));
+
+
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/amazon')
+}
+
+const bookSchema= mongoose.Schema({
+  title: {
+    type:String,
+    required: true,
+  },
+  author:{
+    type:String,
+  },
+  price:{
+    type:Number,
+  },
+});
+
+const book = mongoose.model('book', bookSchema);
