c42ad7b5686c5b6c7443038690eb4205d6de69fc
Parth Tiwari <parthtiwari670@gmail.com>
2025-06-24 01:04:45 +0530

    Add new book entries and save functionality in books.js

--- books.js
+++ books.js
@@ -24,3 +24,41 @@ const bookSchema= mongoose.Schema({
 });
 
 const book = mongoose.model('book', bookSchema);
+
+let book1 = new book({
+  title:"Science XII",
+  author:"ML Agarwal",
+  price:1090,
+});
+
+/*book1.save().then((res)=>{
+  console.log(res);
+}).catch((err)=> console.log(err));*/
+
+
+let book2 = new book({
+  title:"Mathematics XII",
+  author:"ML Agarwal",
+  price:1090,
+});
+
+/*book2.save().then((res)=>{
+  console.log(res);
+}).catch((err)=> console.log(err));*/
+
+/*book.deleteOne({title:"Mathematics XII"}).then((res)=>{
+  console.log(res);
+  
+}).catch((err)=> console.log(err)
+)*/
+
+let book3 = new book({
+  title:"Computer XII",
+  author:"ML Agarwal",
+  price:4800,
+});
+
+book3.save().then((res)=>{
+  console.log(res);
+}).catch((err)=> console.log(err))
+
