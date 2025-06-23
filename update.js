493b0bf577822f830522fbec1f990909f2be3c07
Parth Tiwari <parthtiwari670@gmail.com>
2025-06-24 00:05:43 +0530

    Add initial MongoDB connection and user update functionality

---
+++ update.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+main().then(()=>{
+  console.log('Sucessfully connected!');
+})
+.catch(err=> console.log(err));
+
+
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/Test')
+}
+
+const userSchema = new mongoose.Schema({
+  name:String,
+  email : String,
+  age : Number,
+});
+
+const User = mongoose.model('User' , userSchema);
+
+User.updateOne({name:"Tinku"}, {age:21}).then((res)=>{
+  console.log(res);
+}).catch((err)=> console.log(err));
