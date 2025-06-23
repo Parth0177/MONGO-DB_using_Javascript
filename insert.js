ef4ca72f963bd1764c7b3f00533bef7d58bdbe76
Parth Tiwari <parthtiwari670@gmail.com>
2025-06-23 23:59:21 +0530

    kk

--- insert.js
+++ insert.js
@@ -44,7 +44,7 @@ user2.save().then((res)=>{
 User.insertMany([
   {name:"Sargam", email:"flat69@gmail.com", age:22},
   {name:"Gungun", email:"Nice69@gmail.com", age:23},
-  {name:"Aryan", email:"big69@gmail.com", age:25},
+  {name:"Aryan", email:"big69@gmail.com", age:24},
   {name:"Shivansh", email:"buns69@gmail.com", age:15},
 
 ]).then((res)=>{
